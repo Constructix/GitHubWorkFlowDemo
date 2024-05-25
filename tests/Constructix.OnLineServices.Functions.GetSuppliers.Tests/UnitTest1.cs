260cd6b08a8006cb1df27ad7d65afe6ba373650d
R Jones <rjjones79@hotmail.com>
2024-05-25 17:57:05 +1000

    Added Unit tests and updated workflow

---
+++ tests/Constructix.OnLineServices.Functions.GetSuppliers.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+
+namespace Constructix.OnLineServices.Functions.GetSuppliers.Tests;
+
+public class NumbersTests
+{
+    [Fact]
+    public void AddNumbersTest()
+    {
+        int sum = 1 + 1;
+
+        sum.Should().Be(2);
+    }
+}
